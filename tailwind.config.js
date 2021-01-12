4a895e3b2c6f7500bb4c020377115fd55175f76c
Bryan Oyloe <boyloe@gmail.com>
2021-01-11 23:38:26 -0700

    Add tailwind and import Archivo font

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        "Archivo": ['Archivo', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-italic": ['Archivo-Italic', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-md": ['Archivo-Medium', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-md-italic": ['Archivo-Medium-Italic', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-semibold": ['Archivo-Semibold', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-semibold-italic": ['Archivo-Semibold-Italic', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-bold": ['Archivo-Bold', 'Helvetica', 'Arial', 'sans-serif'],
+        "Archivo-bold-italic": ['Archivo-Bold-Italic', 'Helvetica', 'Arial', 'sans-serif'],
+      },      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
